e43a0b5244455077d10f95a89e5f98eac7c57d6b
greenbender <byron.platt@gmail.com>
2016-11-24 12:41:50 +1100

    Changed name to django-gravy-bitfields
    So it can be uploaded to pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python
-
+import os
 from setuptools import setup, find_packages
 
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
 setup(
-    name='django-bitfield',
-    version='1.9.0',
-    author='DISQUS',
-    author_email='opensource@disqus.com',
-    url='https://github.com/disqus/django-bitfield',
-    description='BitField in Django',
+    name='django-gravy-bitfield',
+    version='1.9.1',
     packages=find_packages(),
-    zip_safe=False,
     install_requires=[
         'Django>=1.8',
         'six',
@@ -24,6 +27,13 @@ setup(
         ],
     },
     include_package_data=True,
+    license='Apache License',
+    description='BitField in Django',
+    long_description=README,
+    author='DISQUS',
+    author_email='opensource@disqus.com',
+    url='https://github.com/disqus/django-bitfield',
+    zip_safe=False,
     classifiers=[
         'Framework :: Django',
         'Intended Audience :: Developers',
